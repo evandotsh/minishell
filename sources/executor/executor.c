a8dd26b6fbf0429a15456701f2085e5276a7cf4f
Evan <me@evan.sh>
2023-06-23 16:29:59 +0200

    ✨ (executor): Minimum viable executor for testing

--- sources/executor/executor.c
+++ sources/executor/executor.c
@@ -6,12 +6,24 @@
 /*   By: evmorvan <evmorvan@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 15:56:28 by evmorvan          #+#    #+#             */
-/*   Updated: 2023/06/22 15:57:31 by evmorvan         ###   ########.fr       */
+/*   Updated: 2023/06/23 15:58:02 by evmorvan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	executor(void)
+void	executor(t_cmd *cmd_list)
 {
+	t_cmd	cmd;
+	int		pid;
+
+	cmd = *cmd_list;
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(cmd.cmd, cmd.args, NULL);
+		sleep(1);
+	}
+	else
+		waitpid(pid, NULL, 0);
 }
