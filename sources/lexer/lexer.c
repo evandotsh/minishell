b7de5da8a3aed124c1a9d5ec12847dc2f8259552
TheBlackBody <66089114+TheBlackBody@users.noreply.github.com>
2023-09-12 00:37:25 +0200

    pb

--- sources/lexer/lexer.c
+++ sources/lexer/lexer.c
@@ -37,20 +37,24 @@ char	*check_redir(char *str, int i, int j, char *result)
 	{
 		if (str[i] == 34 || str[i] == 39)
 			quote(str, i, j, result);
-		if (str[i] == '|' || (str[i] == '>' && str[i - 1] != '>')
+		if ((str[i] == '|' || (str[i] == '>' && str[i - 1] != '>')
 			|| (str[i] == '<' && str[i - 1] != '<'))
+			&& (str[i - 1] != 34 && str[i - 1] != 39))
 		{
 			result[j++] = ' ';
 			if (str[i + 2] == '>' || str[i + 2] == '<')
 				return (NULL);
 		}
-		if (str[i - 1] == '|' || (str[i - 1] == '>' && str[i] != '>')
+		if ((str[i - 1] == '|' || (str[i - 1] == '>' && str[i] != '>')
 			|| (str[i - 1] == '<' && str[i] != '<'))
+			&& (str[i - 1] != 34 && str[i - 1] != 39))
 			result[j++] = ' ';
 		result[j++] = str[i++];
 	}
 	result[j] = '\0';
 	result = ft_strtrim_lexer(result);
+	result = ft_epur_str(result);
+	ft_printf("%s\n", result);
 	return (result);
 }
 
