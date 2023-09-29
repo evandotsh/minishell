fedcd632f90736e5952d18268e6f66f6bdd8a23f
Evan <me@evan.sh>
2023-09-29 16:37:02 +0200

    fix exit code limit

--- sources/builtins/exit.c
+++ sources/builtins/exit.c
@@ -6,17 +6,17 @@
 /*   By: evmorvan <evmorvan@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 13:59:55 by evmorvan          #+#    #+#             */
-/*   Updated: 2023/09/28 14:57:12 by evmorvan         ###   ########.fr       */
+/*   Updated: 2023/09/29 16:36:44 by evmorvan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-unsigned long long	ft_atoull(const char *str)
+long long	ft_atoll(const char *str)
 {
-	int					i;
-	unsigned long long	nb;
-	unsigned long long	sign;
+	int			i;
+	long long	nb;
+	long long	sign;
 
 	i = 0;
 	nb = 0;
@@ -43,8 +43,8 @@ int	ft_is_code_valid(char *str)
 	int	i;
 
 	i = 0;
-	if (ft_atoull(str) > 9223372036854775807ULL
-		|| ft_atoull(str) < -9223372036854775808ULL)
+	if (ft_atoll(str) > 9223372036854775807
+		|| ft_atoll(str) < -9223372036854775807)
 		return (0);
 	if (str[i] == '-' || str[i] == '+')
 		i++;
