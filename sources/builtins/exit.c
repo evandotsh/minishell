dc865170594eb366fd8d1e575d6da73e3d68a1b6
Evan <me@evan.sh>
2023-09-29 16:49:51 +0200

    remove check for long long

--- sources/builtins/exit.c
+++ sources/builtins/exit.c
@@ -6,46 +6,17 @@
 /*   By: evmorvan <evmorvan@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 13:59:55 by evmorvan          #+#    #+#             */
-/*   Updated: 2023/09/29 16:36:44 by evmorvan         ###   ########.fr       */
+/*   Updated: 2023/09/29 16:49:17 by evmorvan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-long long	ft_atoll(const char *str)
-{
-	int			i;
-	long long	nb;
-	long long	sign;
-
-	i = 0;
-	nb = 0;
-	sign = 1;
-	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
-		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign *= -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		nb = nb * 10 + (str[i] - '0');
-		i++;
-	}
-	return (nb * sign);
-}
 
 int	ft_is_code_valid(char *str)
 {
 	int	i;
 
 	i = 0;
-	if (ft_atoll(str) > 9223372036854775807
-		|| ft_atoll(str) < -9223372036854775807)
-		return (0);
 	if (str[i] == '-' || str[i] == '+')
 		i++;
 	if (!str[i])
